4f15539a9e5b39396bcfd2eccced0891070ab0cb
audrius-savickas <umarasss@gmail.com>
2020-12-19 13:22:59 +0200

    Initial commit0

---
+++ 4.c
@@ -0,0 +1,16 @@
+/* Audrius Savickas 4gr 1pogr 4 uzduotis 18 uzdavinys
+
+n18.Sudaryti ciklinį vienpusį sąrašą. Parašyti procedūrą, kuri išmeta
+ reikšme nurodytą elementą. Jeigu tokio elemento nėra, turi būti išvestas
+ atitinkamas pranešimas.
+ 1. inclue *.h, o ne *.c
+2. inicializacimas reiškia tiesiog NULL nustatymas, o ne pirmo nario įdėjimas
+3. Jei yra dvipusis sąrašas, yra būtinos šios komandos: įdėti_į_priekį, įdėti
+_į_galą, atspausdinti_iš_priekio, atspausdinti_iš_galo
+4. Main'e turi būti while su duomenų įterpimu (ir paklausimu ar iš priekio ar
+ iš galo dll atveju)
+5. Atspausdinimas
+6. Tiklsinės funkcijos paleidimas pagal užduotį
+7. atspausdinimas
+8. sunaikinimas
+*/
